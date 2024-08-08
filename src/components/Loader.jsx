d61fd15df62e900b97fb362ada7fa036d5d2a755
shairasaldua <124142782+shairasaldua@users.noreply.github.com>
2024-08-09 03:06:16 +0800

    Added island model

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import { Html } from '@react-three/drei';
 
 const Loader = () => {
     return (
-        <div className="flex justify-center items-center h-screen">
-            <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-slate-900"></div>
-        </div>
+        <Html center>
+            <div className="w-20 h-20 border-2 border-opacity-20 border-blue-500 border-t-blue-500 rounded-full animate-spin"></div>    
+        
+            <div className="flex justify-center items-center h-screen">
+                {/* <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-slate-900"></div> */}
+                <div className="w-20 h-20 border-2 border-opacity-20 border-blue-500 border-t-blue-500 rounded-full animate-spin"></div>
+            </div>
+        </Html>
     );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,8 @@ import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import Loader from "../components/Loader.jsx";
 
+import Island from "../models/Island";
+
 {/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">POPUpz</div> 
      <Canvas>
                 <ambientLight intensity={0.5} />
@@ -17,7 +19,20 @@ import Loader from "../components/Loader.jsx";
                 </mesh>
             </Canvas> */}
 
-const Home = () => {    
+const Home = () => {   
+    const adjustIslandForScreenSize = () => {
+        let screenScale = null
+        let screenPosition = [0, -6.5, -43];
+        let rotation = [0.1, 4.7, 0];
+
+        if (window.innerWidth < 768) {
+            screenScale = [0.9, 0.9, 0.9];
+        } else {
+            screenScale = [1, 1, 1];
+        }
+        return [screenScale, screenPosition];
+    }
+    const [islandScale, islandPosition, rotation] = adjustIslandForScreenSize();
     return (
         <section className="w-full h-screen relative">
             <Canvas 
@@ -25,7 +40,17 @@ const Home = () => {
                 camera={{ near: 0.1, far: 1000 }
             }>
                 <Suspense fallback={<Loader />}>
+                    <directionalLight position={[10, 1, 1]} intensity={2}/>
+                    <ambientLight intensity={0.5}/>
+                    <pointLight position={[10, 10, 10]} />
+                    <spotLight position={[0, 0, 0]} />
+                    <hemisphereLight skyColor="#0d5197" groundColor="#8a523e" intensity={0.75}/>
 
+                    <Island
+                      position={islandPosition}
+                      scale= {islandScale}
+                      rotation={rotation}
+                    />
                 </Suspense>
             </Canvas>
         </section>
